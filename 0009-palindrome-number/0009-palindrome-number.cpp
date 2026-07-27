ec8819cef1c38ac1fddd378d8199bfdbdce6db19
Rahul Devidas Naik <naik2.7rahul@gmail.com>
2026-07-27 23:07:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        unsigned int rev = 0;
-        int c = x;
-
-        while (c > 0) {
-            rev = rev * 10 + c % 10;
-            c /= 10;
+        if (x<0 || (x%10 == 0 && x!=0)) return false;
+        long long rev = 0;
+        while (x>rev) {
+            rev = (rev*10) + (x%10);
+            x/=10;
         }
-
-        return rev == x ? true : false;
+        return (rev == x || x == rev/10);
     }
 };
