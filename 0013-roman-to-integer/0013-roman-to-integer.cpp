0079a914c3d51176accfc206e8bc984cf457d6a2
Rahul Devidas Naik <naik2.7rahul@gmail.com>
2026-07-28 23:03:00 +0530

    Time: 0 ms (100%), Space: 9.9 MB (94.63%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I',1}, {'V',5}, {'X',10}, {'L',50},
-            {'C',100}, {'D',500}, {'M',1000}
-        };
-        
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int curr = roman[s[i]];
-            int next = (i+1 < s.size()) ? roman[s[i+1]] : 0;
-            
-            if (curr < next) {
-                result -= curr;
-            } else {
-                result += curr;
-            }
-        }
-        return result;
+int romanToInt(std::string s) {
+  int res = 0;
+  char prev = 0;
+  for (auto it = s.rbegin(); it < s.rend(); ++it) {
+    char cur = *it;
+    switch (cur) {
+    case 'I':
+      (prev == 'V' || prev == 'X') ? res -= 1 : res += 1;
+      break;
+    case 'V':
+      res += 5;
+      break;
+    case 'X':
+      (prev == 'L' || prev == 'C') ? res -= 10 : res += 10;
+      break;
+    case 'L':
+      res += 50;
+      break;
+    case 'C':
+      (prev == 'D' || prev == 'M') ? res -= 100 : res += 100;
+      break;
+    case 'D':
+      res += 500;
+      break;
+    case 'M':
+      res += 1000;
+      break;
     }
+    prev = cur;
+  }
+
+  return res;
+}
 };
