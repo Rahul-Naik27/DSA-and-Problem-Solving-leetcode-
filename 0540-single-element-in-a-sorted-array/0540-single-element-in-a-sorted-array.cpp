9ebc6fe2ea8d6f59b3f6b263722cb50f27d05234
Rahul Devidas Naik <naik2.7rahul@gmail.com>
2026-08-27 00:08:40 +0530

    Refactor singleNonDuplicate function for clarity

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& A) {
-        int n = A.size();
+        int n=A.size();
 
-        if(n == 1) return A[0];
+        if(n==1) return A[0];
 
-        int st = 0, end = n-1;
-        while(st <= end){
-            int mid = st + (end-st)/2;
+        int st = 0, end=n-1;
+        while(st<=end){
+            int mid = st+(end-st)/2;
 
-            if(mid == 0 && A[0] != A[1]) return A[mid];
-            if(mid == n-1 && A[n-1] != A[n-2]) return A[mid];
+            if(mid==0 && A[0]!=A[1]) return A[mid];
+            if(mid==n-1 && A[n-1]!=A[n-2]) return A[mid];
 
-            if(A[mid-1] != A[mid] && A[mid] != A[mid+1]) return A[mid];
+            if(A[mid-1]!=A[mid] && A[mid]!=A[mid+1]) return A[mid];
 
-            if(mid % 2 == 0){
-                if(A[mid-1] == A[mid]){
-                    end = mid-1;
+            if(mid%2==0){
+                if(A[mid-1]==A[mid]){
+                    end=mid-1;
                 }else{
-                    st = mid+1;
+                    st=mid+1;
                 }
             }else{
-                if(A[mid-1] == A[mid]){
-                    st = mid+1;
+                if(A[mid-1]==A[mid]){
+                    st=mid+1;
                 }else{
-                    end = mid-1;
+                    end=mid-1;
                 }
             }
         }
